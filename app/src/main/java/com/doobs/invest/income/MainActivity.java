a6b10c0237f54ee98f1353d6f63df3e18128b144
mduby <mduby@broadinstitute.org>
2018-11-24 00:11:56 -0500

    T-04: added main activity toolbar

--- app/src/main/java/com/doobs/invest/income/MainActivity.java
+++ app/src/main/java/com/doobs/invest/income/MainActivity.java
@@ -9,7 +9,9 @@ import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -37,6 +39,9 @@ public class MainActivity extends AppCompatActivity implements PortfolioRecycler
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         // log
         Log.i(this.TAG_NAME, "In onCreate");
